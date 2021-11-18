8765a76adddc605be1be7e4be4870779dd921e5a
Rushikesh Mungse <mungse.rushi@gmail.com>
2021-11-18 09:31:48 +0530

    initial

---
+++ server.js
@@ -0,0 +1,23 @@
+//import express function
+const express = require('express')
+const app = express()
+const PORT = process.env.PORT || 3000
+
+const path = require('path')
+const ejs = require('ejs')
+const ejsLayouts = require('express-ejs-layouts')
+
+app.use(express.static('public'))
+app.use(ejsLayouts)
+app.set('views', path.join(__dirname, 'resources/views'))
+app.set('view engine', 'ejs')
+
+//route
+app.get('/', (req, res) => {
+    res.render('home')
+})
+
+//create server using express
+app.listen(PORT, () => {
+    console.log(`Server listening on port ${PORT}`)
+})
