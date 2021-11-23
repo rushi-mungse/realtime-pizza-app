70a3fe381644a850dc7b731c8f89c069805170a1
Rushikesh Mungse <mungse.rushi@gmail.com>
2021-11-23 13:51:03 +0530

    menu featched from mongoDb database

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 //import express function
+require('dotenv').config()
 const express = require('express')
 const app = express()
 const PORT = process.env.PORT || 3000
@@ -6,12 +7,29 @@ const PORT = process.env.PORT || 3000
 const path = require('path')
 const ejs = require('ejs')
 const ejsLayouts = require('express-ejs-layouts')
+const mongoose = require('mongoose')
 
 
 app.use(express.static('public'))
 app.use(ejsLayouts)
 app.set('views', path.join(__dirname, 'resources/views'))
 app.set('view engine', 'ejs')
+
+//mongoose connection
+function DbConnected() {
+    mongoose.connect(process.env.MONGO_URL, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        // useFindAndModify: false
+    });
+
+    const db = mongoose.connection;
+    db.on('error', console.error.bind(console, 'connection error:'));
+    db.once('open', () => {
+        console.log('DB Connected..')
+    });
+}
+DbConnected()
 
 //route
 require('./routes/web')(app)
